c8fc5b746671b7cf0a27945eed5a5beb5019372a
Obiokaforvictor <obiokaforvictor8@gmail.com>
2026-05-02 17:55:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let cart = [];
 
+function addToCart(productName, price) {
+    cart.push({ name: productName, price: price });
+    displayCart();
+}
+
+function displayCart() {
+    let cartList = document.getElementById("cart-list");
+    let total = 0;
+
+    cartList.innerHTML = "";
+
+    cart.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item.name + " - $" + item.price;
+        cartList.appendChild(li);
+
+        total += item.price;
+    });
+
+    document.getElementById("total").textContent = total;
+}
